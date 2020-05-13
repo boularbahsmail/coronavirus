fe80f04cdb534d09455f83fa5f5539722059f4bb
ismail boularbah <qiranorane55@gmail.com>
2020-05-13 22:33:59 +0000

    comments

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
+// select items
 const selectElem = document.querySelector('select#select');
-
+// new option
 function newOption(data) {
+    // get title of data
     const title = data.title;
     if (typeof title != "undefined") {
+        // create new element : option
         const optionBox = document.createElement("option");
+        // countires
         optionBox.innerHTML = title;
+        // append the new element
         selectElem.appendChild(optionBox);
     }
 }
-
+// fetch data _ singleData / manipuate data
 function manipulateData(data) {
+    // get country items : cases, deaths ....
     for  (eachItem in data.countryitems[0]){
         const signleData = data.countryitems[0][eachItem];
         newOption(signleData);
         selectElem.addEventListener('change', function(e) {
-            // console.log(data);
+            // choose country => show single data
             if(e.target.value == signleData.title) {
                 let totalCases = signleData.total_cases;
                 let totalRecovered = signleData.total_recovered;
@@ -25,7 +31,7 @@ function manipulateData(data) {
                 let totalActive_today = signleData.total_active_cases;
                 let totalSerious = signleData.total_serious_cases;
 
-
+                // divs
                 let cardTemplate = `
                 <div class="row justify-content-center">
                     <div class="card pl-3 pr-5 py-2 mx-2 my-2 col-md-3 bg-brown">
@@ -68,17 +74,22 @@ function manipulateData(data) {
                         <p>${totalSerious}</p>
                     </div>
                 `;
-
+                // insert divs elements
                 const wrapper = document.querySelector('.wrapper');
                 wrapper.innerHTML = cardTemplate;
             }
         });
     }
 }
-
+// fetch API
 const fetchData = fetch('https://api.thevirustracker.com/free-api?countryTotals=ALL')
-.then(response => {return response.json();
+.then(response => {    // handle response data
+    return response.json();
 })
 .then(data => {
     manipulateData(data);
+})
+.catch(err => {
+    // if there is an error in fetching data
+    err.alert("Feiled to fetch data !");
 })
