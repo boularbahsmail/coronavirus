ee973ddfd42ecb585309b7eec1d71fd3366149d9
ismail boularbah <qiranorane55@gmail.com>
2020-05-18 23:27:09 +0000

    colors

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function manipulateData(data) {
                     </div>
 
                     <div class="card pl-3 pr-5 py-2 mx-2 my-2 col-md-3 bg-brown">
-                    <h2 class="mb-2">Unresolved</h2>
+                    <h2 class="mb-2">Unresolved cases</h2>
                     <p>${totalUnresolved}</p>
                 </div>
 
