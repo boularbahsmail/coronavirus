c8cdbaee2822dae82405c4ce99f799fa228dd09c
boularbahsmail <obcodex@gmail.com>
2020-10-27 00:32:00 +0100

    add transclator

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function manipulateData(data) {
                     </div>
 
                     <div class="card pl-3 pr-5 py-2 mx-2 my-2 col-md-3 bg-brown" id="card">
-                        <h2 class="mb-2" id="recovered">Total <br> recovered</h2>
+                        <h2 class="mb-2" id="recovered">Total <br> recovered cases</h2>
                         <h5 id='all'>${totalRecovered}</h5>
                     </div>
                 
@@ -55,12 +55,12 @@ function manipulateData(data) {
                     </div>
 
                     <div class="card pl-3 pr-5 py-2 mx-2 my-2 col-md-3 bg-brown" id="card">
-                        <h3 class="mb-2">New cases for today</h3>
+                        <h3 class="mb-2" id="cases">New cases for today</h3>
                         <h5 id='all'>${total_new_cases_today}</h5>
                     </div>
 
                     <div class="card pl-3 pr-5 py-2 mx-2 my-2 col-md-3 bg-brown" id="card">
-                        <h3 class="mb-2">New deaths for today</h3>
+                        <h3 class="mb-2" id="deaths">New deaths for today</h3>
                         <h5 id='all'>${total_new_deaths_today}</h5>
                     </div>
 
@@ -111,3 +111,64 @@ const sl = document.getElementById("select").addEventListener('click', () => {
 })
 
 
+
+// Translator
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+  }
+  
+  // Minified scripts
+  (function(){var gtConstEvalStartTime = new Date();
+  
+  function d(b) {
+    var a=document.getElementsByTagName("head")[0];
+    a||(a=document.body.parentNode.appendChild(document.createElement("head")));
+    a.appendChild(b)}function _loadJs(b){var a=document.createElement("script");
+    a.type="text/javascript";a.charset="UTF-8";a.src=b;d(a)
+  }
+  // load CSS => meta charset => UTF-8
+  function _loadCss(b) {
+    var a=document.createElement("link");
+    a.type="text/css";a.rel="stylesheet";
+    a.charset="UTF-8";a.href=b;d(a)
+  }
+  function _isNS(b) {
+    b=b.split(".");
+    for(var a=window,c=0;c<b.length;++c)
+      if(!(a=a[b[c]])) return!1; return!0
+  }
+  function _setupNS(b) {
+    b=b.split(".");
+    for(var a=window,c=0;c<b.length;++c)
+      a.hasOwnProperty?a.hasOwnProperty(b[c])?a=a[b[c]]:a=a[b[c]]={}:a=a[b[c]]||(a[b[c]]={});
+    return a
+  }
+  window.addEventListener&&"undefined"==typeof document.readyState&&window.addEventListener("DOMContentLoaded",function() {
+    document.readyState="complete"},!1);
+  if (_isNS('google.translate.Element')) {
+    return
+  }
+  // Get google translator api
+  (function() {
+    var c=_setupNS('google.translate._const');c._cest = gtConstEvalStartTime;
+    gtConstEvalStartTime = undefined;c._cl='en';
+    c._cuc='googleTranslateElementInit';
+    c._cac='';
+    c._cam='';
+    c._ctkk='440335.1449305758';
+    var h='translate.googleapis.com';
+    var s=(true?'https':window.location.protocol=='https:'?'https':'http')+'://';
+    // Translate static files    
+    var b=s+h;c._pah=h;c._pas=s;c._pbi=b+'/translate_static/img/te_bk.gif';
+    c._pci=b+'/translate_static/img/te_ctrl3.gif';
+    c._pli=b+'/translate_static/img/loading.gif';
+    c._plla=h+'/translate_a/l';
+    c._pmi=b+'/translate_static/img/mini_google.png';
+    c._ps=b+'/translate_static/css/translateelement.css';
+    c._puh='translate.google.com';_loadCss(c._ps);
+    _loadJs(b+'/translate_static/js/element/main.js');
+  })
+  ();
+  })
+  ();
